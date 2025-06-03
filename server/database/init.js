5ab37fdbe927bc314bd19059b69b49f0bde7a179
Purin Liang <purinliang@gmail.com>
2025-06-03 20:47:09 +1000

    feature: create database

--- server/database/init.js
+++ server/database/init.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./techblog.db');
+const db = new sqlite3.Database('./database/techblog.db');
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS posts (
