979ae2607d20f9c7f8eccb43e6179bb4ffaa0470
Purin Liang <purinliang@gmail.com>
2025-06-04 14:46:57 +1000

    feature: update home ui

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,12 @@
-import PostList from '../components/PostList';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import PostList from "../components/PostList";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 export default function Home() {
   return (
-    <Box sx={{ maxWidth: 900, mx: 'auto', mt: 4 }}>
-      <Typography variant="h4" fontWeight="600" mb={3} color="primary">
-        All Posts
+    <Box sx={{ maxWidth: 800, mx: "auto", mt: 6 }}>
+      <Typography variant="h5" fontWeight="600" mb={2} color="primary">
+        Post List
       </Typography>
       <PostList />
     </Box>
