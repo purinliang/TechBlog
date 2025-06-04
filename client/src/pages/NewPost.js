7fa9bcd8347254ad539ac81893127a60e0644c48
Purin Liang <purinliang@gmail.com>
2025-06-04 14:47:13 +1000

    feature: update new post ui

--- client/src/pages/NewPost.js
+++ client/src/pages/NewPost.js
@@ -1,15 +1,15 @@
-import PostForm from '../components/PostForm';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
+import PostForm from "../components/PostForm";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 
 export default function NewPost() {
   return (
-    <Box sx={{ maxWidth: 700, mx: 'auto', mt: 4, p: 3 }}>
-      <Typography variant="h4" fontWeight="600" mb={3} color="primary">
+    <Box sx={{ maxWidth: 800, mx: "auto", mt: 6 }}>
+      <Typography variant="h5" fontWeight="600" mb={3} color="primary">
         Create a New Post
       </Typography>
-      <Paper elevation={3} sx={{ p: 3 }}>
+      <Paper sx={{ p: 3, cursor: "pointer", "&:hover": { boxShadow: 8 } }}>
         <PostForm />
       </Paper>
     </Box>
