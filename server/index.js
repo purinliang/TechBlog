9814eb8adafeede3353e8b6fc094724f22ec8382
Purin Liang <purinliang@gmail.com>
2025-06-04 13:35:34 +1000

    bugfix: port use env

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/posts", postRoutes);
 
-app.listen(5000, "0.0.0.0", () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
