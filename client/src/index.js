a69fd07730bb03975aef1828b88b159e554aabde
Purin Liang <purinliang@gmail.com>
2025-06-10 00:25:09 +1000

    bugfix: cannot refresh

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import MyPostsPage from "./pages/MyPostsPage";
