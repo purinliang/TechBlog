afd8e89f225eaa65f335d17b4ac240f38edb7416
Purin Liang <purinliang@gmail.com>
2025-06-09 12:58:00 +1000

    bugfix: add a loading tips

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import PostList from "../components/PostList";
 import { getPosts } from "../apis/postApi";
-import { CircularProgress, Box, Alert } from "@mui/material";
+import { CircularProgress, Typography, Box, Alert } from "@mui/material";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
@@ -35,6 +35,9 @@ export default function Home() {
         }}
       >
         <CircularProgress />
+        <Typography variant="body" color="text.secondary" mt={2}>
+          It may take around 15 seconds to start the backend if it's been idle.
+        </Typography>
       </Box>
     );
   }
